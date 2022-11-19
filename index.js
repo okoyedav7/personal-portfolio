400eff329b565a88004476c1284de702b9ce1dfb
okoyedav7 <okoyeda7@gmail.com>
2022-11-19 05:06:09 +0100

    second commit

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
 let width = screen.availWidth;
 
+$(".menu-toggle").click(function () {
+  $("#mobile-menu").toggleClass("is-active");
+  $("ul").toggleClass("active");
+});
 
+$(".nav-item").click(function () {
+  $("ul").removeClass("active");
+  $("#mobile-menu").toggleClass("is-active");
+});
 
-
- $('.menu-toggle').click(function(){
-     $('#mobile-menu').toggleClass("is-active");
-     $('ul').toggleClass("active");
-  });
-
-  $(".nav-item").click(function(){
-    $("ul").removeClass("active");
-    $('#mobile-menu').toggleClass("is-active");
-  });
-  
-
-
-
-  var swiper = new Swiper('.swiper', {  
-    slidesPerView: width > 480 ? "3" : "1",
-    spaceBetween: 30,
-    speed: 600,
-    parallax: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-
+var swiper = new Swiper(".swiper", {
+  slidesPerView: width > 480 ? "3" : "1",
+  spaceBetween: 30,
+  speed: 600,
+  parallax: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
